2bf4a55d51a134869691b016d0f59833479d9ffa
srodman1 <77469278+srodman1@users.noreply.github.com>
2022-04-25 16:59:08 -0500

    submodule fix

--- defect-fix-2/angry-birds-demo-16-17 2/AngryBirds/Views/BirdCell.swift
+++ defect-fix-2/angry-birds-demo-16-17 2/AngryBirds/Views/BirdCell.swift
@@ -21,9 +21,9 @@ class BirdCell: UITableViewCell {
             self.accessoryType = bird!.confirmedSighting ? .checkmark : .none
             
             DispatchQueue.global(qos: .userInitiated).async {
-                let birdImageData = NSData(contentsOf: URL(string: self.bird!.imageUrl)!)
+                guard let birdImageData = NSData(contentsOf: URL(string: self.bird!.imageUrl)!) else {return} // this was the key change I made to adjust the issue
                 DispatchQueue.main.async {
-                    self.birdImageView.image = UIImage(data: birdImageData as! Data)
+                     self.birdImageView.image = UIImage(data: birdImageData as! Data)
                     self.birdImageView.layer.cornerRadius = self.birdImageView.frame.width / 2
                 }
             }
